aafdac9ef328ca1acbaf0d069327b8fc8f2eaa6b
Mich-Tapawan <mik.tapawan@gmail.com>
2025-05-21 00:25:30 +0800

    Adjusted file sources

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from dfa import is_string_valid
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return "Hello, world!"
+    return render_template('index.html') # Render the index.html template
 
-@app.route('/validateString')
+@app.route('/validateString', methods=['POST'])
 def validate_string():
-    return
+    try:
+        data = request.get_json()
+        text = data.get('text')
+        response = is_string_valid(text)
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': f'Internal server error: {str(e)}'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True) #runs the flask application.
---
+++ dfa.py
@@ -0,0 +1,2 @@
+def is_string_valid(string:str):
+    return
