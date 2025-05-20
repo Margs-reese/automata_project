6f46db624d95f2c115d0105efafd2bd581819033
Margs-reese <CMR1030@dlsud.edu.ph>
2025-05-20 17:21:50 +0800

    added initial button printed compiler message

---
+++ src/components/functions/simulation_btn.js
@@ -0,0 +1,9 @@
+// run simulation button id = 'run-simulation-btn' (from compiler.html)
+const runSimulationButton = document.getElementById('run-simulation-btn');
+
+if (runSimulationButton) {
+    runSimulationButton.addEventListener('click', () => {
+        console.log('simulation button clicked');  // msg printed when run simulation btn is clicked (for debugging)
+        // add func. here
+    });
+}
