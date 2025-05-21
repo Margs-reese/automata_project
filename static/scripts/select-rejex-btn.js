0a95ae197c25810acabd77b57fc52f4e78f95932
Margs-reese <CMR1030@dlsud.edu.ph>
2025-05-21 14:36:12 +0800

    deleted old regex btn js file

--- static/scripts/select-rejex-btn.js
+++
@@ -1,9 +0,0 @@
-// select rejex button id = 'select-rejex-btn' (from compiler.html)
-const selectRejexButton = document.querySelector('#select-rejex-btn');
-
-if (selectRejexButton) {
-    selectRejexButton.addEventListener('click', () => {
-        console.log('Select Rejex button clicked!'); // msg printed when run simulation btn is clicked (for debugging)
-        // Add func. here
-    });
-}
